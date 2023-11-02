809337af6e3459d04f9e869b1b042e69afc7595a
Joshua M. Keyes <joshua.michael.keyes@gmail.com>
2023-11-02 16:14:29 -0700

    gyroflow-openfx-plugin 1.3.0

--- Casks/gyroflow-openfx-plugin.rb
+++ Casks/gyroflow-openfx-plugin.rb
@@ -1,12 +1,17 @@
 cask "gyroflow-openfx-plugin" do
-  version "1.1"
-  sha256 "f1305906ffe2ee70906ea2c5e4072d799670056fe7bdf14c493037eec04a4313"
+  version "1.3.0"
+  sha256 "4f364d634029bba72eadb6245cd22f4cb40627eded2eb41c3ef573308774d6ea"
 
   url "https://github.com/gyroflow/gyroflow-ofx/releases/download/v#{version}/gyroflow-ofx-macosx.dmg",
       verified: "github.com/gyroflow/gyroflow-ofx/"
   name "Gyroflow OpenFX Plugin"
   desc "OpenFX plugin for Gyroflow video stabilization"
   homepage "https://gyroflow.xyz/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   depends_on macos: ">= :mojave"
 
